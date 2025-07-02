a54581d0796a6de4920b84cfecc1c5c408f15a83
Melik <melikpapikyan1@gmail.com>
2025-07-02 16:34:59 +0400

    weather-app

--- weather/js/app.js
+++ weather/js/app.js
@@ -3,6 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const menuBtn = document.querySelector('.header__icon-cover.menu');
     const menu = document.querySelector('header .settings');
+    const modal = document.querySelector('body .modal');
+    const modalCancel = document.querySelector('body .modal .modal__cancel');
+
+
+    const submenu = document.querySelector('.submenu');
+    const darkTheme = document.querySelector('.submenu .dark');
+    const lightTheme = document.querySelector('.submenu .light');
+
+      const darkIcon = document.querySelector('.submenu .dark i');
+    const lightIcon = document.querySelector('.submenu .light i');
 
     const body = document.body;
 
@@ -22,10 +32,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     weatherSettings.addEventListener('click', () => {
-        console.log('settings');
+        body.classList.add('lock');
+        modal.classList.add('reveal');
+    });
+
+    modalCancel.addEventListener('click', () => {
+        body.classList.remove('lock');
+        modal.classList.remove('reveal');
     });
 
 
+    submenu.addEventListener('click', (e) => {
+        const target = e.target;
+        if (darkTheme.contains(target)) {
+            darkIcon.classList.add('fa-check');
+            lightIcon.classList.remove('fa-check');
+        }
+
+        if (lightTheme.contains(target)) {
+             lightIcon.classList.add('fa-check');
+             darkIcon.classList.remove('fa-check');
+        }
+    });
 
 
 });
